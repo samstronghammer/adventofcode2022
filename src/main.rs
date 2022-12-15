60f986174b8125dc6bbb9d422c6c0f0251491b6b
samstronghammer <oliphantsb@gmail.com>
2022-12-14 22:04:08 -0500

    Day 14

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@
 // mod day10;
 // mod day11;
 // mod day12;
-mod day13;
+// mod day13;
+mod day14;
 mod util;
 
 fn main() {
@@ -26,5 +27,6 @@ fn main() {
   // day10::run();
   // day11::run();
   // day12::run();
-  day13::run();
+  // day13::run();
+  day14::run();
 }
