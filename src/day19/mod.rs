5f0183bc625b806182fe0076b3d912a6e4b1a7db
samstronghammer <oliphantsb@gmail.com>
2022-12-19 11:47:05 -0500

    day 19 stub

---
+++ src/day19/mod.rs
@@ -0,0 +1,15 @@
+use crate::util::advent;
+
+pub fn run() {
+  let contents = advent::parse_input(19);
+  let lines: Vec<&str> = contents.split("\n").collect();
+
+  for line in lines {
+    let toks: Vec<&str> = line.split(',').collect();
+  }
+
+  println!("Part 1:");
+  println!("{}", 1);
+  println!("Part 2:");
+  println!("{}", 2);
+}
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@
 // mod day15;
 // mod day16;
 // mod day17;
-mod day18;
+// mod day18;
+mod day19;
 mod util;
 
 fn main() {
@@ -36,5 +37,6 @@ fn main() {
   // day15::run();
   // day16::run();
   // day17::run();
-  day18::run();
+  // day18::run();
+  day19::run();
 }
