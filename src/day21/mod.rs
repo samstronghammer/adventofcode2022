d2a51a8e269ca95d77cea2471bef620969b85bc8
samstronghammer <oliphantsb@gmail.com>
2022-12-21 17:29:22 -0500

    Day 21 improvements

--- src/day21/mod.rs
+++ src/day21/mod.rs
@@ -11,25 +11,91 @@ struct Monkey {
 #[derive(Clone)]
 struct Equation(String, char, String);
 
-fn calc_equation(monkeys: &HashMap<String, Monkey>, equation: Equation) -> i64 {
-  let left = calc_monkey(monkeys, equation.0.clone());
-  let right = calc_monkey(monkeys, equation.2.clone());
-  return match equation.1 {
-    '*' => left * right,
-    '+' => left + right,
-    '-' => left - right,
-    '/' => left / right,
-    '=' => left - right, // Providing the difference is more information, which is helpful for searching the space
-    _ => panic!("unknown operand"),
-  };
+fn calc_equation(monkeys: &HashMap<String, Monkey>, equation: Equation) -> Option<i64> {
+  let left_option = calc_monkey(monkeys, equation.0.clone());
+  let right_option = calc_monkey(monkeys, equation.2.clone());
+  if left_option.is_some() && right_option.is_some() {
+    let left = left_option.unwrap();
+    let right = right_option.unwrap();
+    return Some(match equation.1 {
+      '*' => left * right,
+      '+' => left + right,
+      '-' => left - right,
+      '/' => left / right,
+      '=' => {
+        if left == right {
+          1
+        } else {
+          0
+        }
+      }
+      _ => panic!("unknown operand"),
+    });
+  }
+  return None;
 }
 
-fn calc_monkey(monkeys: &HashMap<String, Monkey>, id: String) -> i64 {
+fn calc_monkey(monkeys: &HashMap<String, Monkey>, id: String) -> Option<i64> {
   let monkey = &monkeys[&id];
-  if monkey.value.is_some() {
-    return monkey.value.unwrap();
+  if monkey.equation.is_some() {
+    return calc_equation(monkeys, monkey.equation.clone().unwrap());
   }
-  return calc_equation(monkeys, monkey.equation.clone().unwrap());
+  return monkey.value;
+}
+
+fn calc_expectation(monkeys: &HashMap<String, Monkey>, equation: Equation, expected: i64) -> i64 {
+  let left = calc_monkey(monkeys, equation.0.clone());
+  let right = calc_monkey(monkeys, equation.2.clone());
+  if (left.is_some() && right.is_some()) || (left.is_none() && right.is_none()) {
+    panic!("Must have exactly one side be none");
+  }
+  let (next_expectation, recurse_id) = match equation.1 {
+    '*' => {
+      if left.is_none() {
+        (expected / right.unwrap(), equation.0.clone())
+      } else {
+        (expected / left.unwrap(), equation.2.clone())
+      }
+    }
+    '+' => {
+      if left.is_none() {
+        (expected - right.unwrap(), equation.0.clone())
+      } else {
+        (expected - left.unwrap(), equation.2.clone())
+      }
+    }
+    '-' => {
+      if left.is_none() {
+        (expected + right.unwrap(), equation.0.clone())
+      } else {
+        (left.unwrap() - expected, equation.2.clone())
+      }
+    }
+    '/' => {
+      if left.is_none() {
+        (expected * right.unwrap(), equation.0.clone())
+      } else {
+        (left.unwrap() / expected, equation.2.clone())
+      }
+    }
+    '=' => {
+      // Ignores expected value, because we know it must be 1.
+      if left.is_none() {
+        (right.unwrap(), equation.0.clone())
+      } else {
+        (left.unwrap(), equation.2.clone())
+      }
+    }
+    _ => panic!("unknown operand"),
+  };
+  if recurse_id == "humn" {
+    return next_expectation;
+  }
+  let next_equation = &monkeys[&recurse_id].equation;
+  if next_equation.is_none() {
+    panic!("humn must be below equations");
+  }
+  return calc_expectation(monkeys, next_equation.clone().unwrap(), next_expectation);
 }
 
 pub fn run() {
@@ -67,23 +133,16 @@ pub fn run() {
 
   let mut monkeys_p2 = monkeys.clone();
   let root = monkeys_p2.get_mut(&"root".to_string()).unwrap();
-  root.equation = Some(Equation(
+  let root_equation = Equation(
     root.equation.clone().unwrap().0,
     '=',
     root.equation.clone().unwrap().2,
-  ));
+  );
+  root.equation = Some(root_equation.clone());
+  monkeys_p2.get_mut(&"humn".to_string()).unwrap().value = None;
 
   println!("Part 1:");
-  println!("{}", calc_monkey(&monkeys, "root".to_string()));
+  println!("{}", calc_monkey(&monkeys, "root".to_string()).unwrap());
   println!("Part 2:");
-  let mut i = 3441198825701; // I found this magic number by doing manual binary search, narrowing the search space by orders of magnitude at a time until I found something close enough that it was able to find the solution quickly.
-  loop {
-    monkeys_p2.get_mut(&"humn".to_string()).unwrap().value = Some(i);
-    let result = calc_monkey(&monkeys_p2, "root".to_string());
-    if result == 0 {
-      println!("{}", i);
-      break;
-    }
-    i += 1;
-  }
+  println!("{}", calc_expectation(&monkeys_p2, root_equation, 0));
 }
